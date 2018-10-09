6924400fd0f99b53d094972f718076e41442c904
prashdeep <pradeep.kumar44@gmail.com>
2018-10-09 14:13:58 +0530

    committing the aspects

--- src/main/java/com/eurofins/aspects/LoggingAspect.java
+++ src/main/java/com/eurofins/aspects/LoggingAspect.java
@@ -19,23 +19,23 @@ public class LoggingAspect {
 		System.out.println(joinPoint);
 	}
 
-	//@After("execution(* com.eurofins.service.*.Employee*())")
+	@After("execution(* com.eurofins.service.*.Employee*())")
 	public void logAfter(JoinPoint joinPoint) {
 		System.out.println("logging after the method has executed");
 	}
 
-	//@Before("allServiceMethodsPointcut()")
+	@Before("allServiceMethodsPointcut()")
 	public void allServiceMethodsAdvice(){
 		System.out.println("Before executing the service method");
 	}
 
-	//@AfterReturning(pointcut = "execution(* findAllEmployees())", returning = "returnList")
+	@AfterReturning(pointcut = "execution(* findAllEmployees())", returning = "returnList")
 	public void getNameReturningAdvice(List<Employee> returnList) {
 		System.out.println("findAllEmployees executed. Returned List=" + returnList);
 	}
 
 	// Pointcut to execute on all the methods of classes in a package
-	//@Pointcut("within(com.eurofins.service.*)")
+	@Pointcut("within(com.eurofins.service.*)")
 	public void allServiceMethodsPointcut() {
 	}
 
